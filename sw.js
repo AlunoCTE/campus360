dd81d197a6907354c3946a485a35f115f1c683c7
AlunoCTE <cte_alunos@aecister.pt>
2026-03-07 18:49:06 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
-const cacheName = 'campus360-v1';
+const cacheName = 'esdica360-v1';
+
 const assets = [
   './',
   './index.html',
-  './assets/icon.png'
+  './manifest.json',
+  './sw.js',
+  // Ficheiros na pasta raíz
+  './README.txt',
+  // Ficheiros da pasta 'assets'
+  './assets/icon.png',
+  './assets/css/style.css', // Confirmar se o nome é este
+  './assets/js/script.js',  // Confirmar se o nome é este
+  // Todas as tuas páginas da pasta 'salas'
+  './salas/atrio.html',
+  './salas/bar.html',
+  './salas/biblioteca.html',
+  './salas/campo-polidesportivo.html',
+  './salas/campos.html',
+  './salas/cantina.html',
+  './salas/ginasio.html',
+  './salas/laboratorio-cn.html',
+  './salas/laboratorio-fq.html',
+  './salas/oficina-informatica.html',
+  './salas/oficina-multimedia.html',
+  './salas/reprografia.html',
+  './salas/salas-cte.html',
+  './salas/salas.html'
 ];
 
-// Instalar o Service Worker e guardar ficheiros na cache
+// Instalação e Cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -14,7 +37,7 @@ self.addEventListener('install', event => {
   );
 });
 
-// Responder mesmo quando estiver offline
+// Interceção de pedidos (Modo Offline)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
